12dfeec5ff734af4ce5a8136af6c92f4df6c05a7
xtypercode <xtyper.code@gmail.com>
2026-07-07 11:35:04 +0100

    fixed

--- includes/user.h
+++ includes/user.h
@@ -7,17 +7,10 @@
 
 typedef struct User
 {
-<<<<<<< HEAD:user.h
     int id;
     int blocked;
     char name[100];
     FileList *files;
-=======
-    int     id;
-    int     blocked;
-    char    name[100];
-    FileList    *files;
->>>>>>> 8b4f1014795bc3245c8d0ce4b8dc317e1135bbdf:includes/user.h
     // Message *inbox;
     // Share   *shares;
 } User;
